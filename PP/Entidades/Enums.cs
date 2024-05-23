43c1446dcb4733a7d3f1e9b162064da3c28c80e6
Santiago Ambricca <117587525+SAmbricca@users.noreply.github.com>
2024-05-22 22:16:25 -0300

    Create Enums.cs

---
+++ PP/Entidades/Enums.cs
@@ -0,0 +1,23 @@
+namespace Entidades
+{
+    public enum ETipoProducto
+    {
+        Tecnologico,
+        Rural,
+        Varios
+    }
+
+    public enum EPaises
+    {
+        China,
+        Taiwan,
+        UnionEuropea
+    }
+
+    public enum EComercios
+    {
+        Importador,
+        Exportador,
+        Ambos
+    }
+}
