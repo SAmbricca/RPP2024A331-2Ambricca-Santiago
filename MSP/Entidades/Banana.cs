0c41a31a4884dcf1a5bb8c42b98251fe3256a66b
Santiago Ambricca <santiagoambricca@hotmail.com>
2024-07-10 16:38:23 -0300

    prueba

---
+++ MSP/Entidades/Banana.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Banana : Fruta
+    {
+        protected string _paisOrigen;
+
+        public string Nombre
+        {
+            get { return "Banana"; }
+        }
+
+        public string PaisOrigen
+        {
+            get
+            {
+                return _paisOrigen;
+            }
+            set
+            {
+                _paisOrigen = value;
+            }
+        }
+
+        public override bool TieneCarozo
+        {
+            get { return false; }
+            set {; }
+        }
+        public Banana() { }
+
+        public Banana(string color, double peso, string paisOrigen)
+            : base(color, peso)
+        {
+            _paisOrigen = paisOrigen;
+        }
+
+        protected override string FrutasToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Fruta: {Nombre}");
+            sb.AppendLine(base.FrutasToString());
+            sb.AppendLine($"Pais de origen: {_paisOrigen}");
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return FrutasToString();
+        }
+    }
+}
---
+++ MSP/Entidades/Durazno.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Durazno : Fruta
+    {
+        protected int _cantPelusa;
+
+        public string Nombre
+        {
+            get { return "Durazno"; }
+        }
+
+        public int CantidadPelusa
+        {
+            get
+            {
+                return _cantPelusa;
+            }
+            set
+            {
+                _cantPelusa = value;
+            }
+        }
+
+        public override bool TieneCarozo
+        {
+            get { return true; }
+            set {; }
+        }
+        public Durazno() { }
+
+        public Durazno(string color, double peso, int cantPelusa)
+            : base(color, peso)
+        {
+            _cantPelusa = cantPelusa;
+        }
+
+        protected override string FrutasToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Fruta: {Nombre}");
+            sb.AppendLine(base.FrutasToString());
+            sb.AppendLine($"Pais de origen: {_cantPelusa}");
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return FrutasToString();
+        }
+    }
+}
